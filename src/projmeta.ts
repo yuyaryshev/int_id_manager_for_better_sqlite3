b4e57b274843d77d1ea9bfcb8b471fe6333bd07d
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-09-06 20:16:16 +0300

    push_all

--- src/projmeta.ts
+++ src/projmeta.ts
@@ -1,2 +1,2 @@
-export const version = '0.0.2';
-export const packageName = 'int_id_manager_for_better_sqlite3';
+export const version = "0.0.2";
+export const packageName = "int_id_manager_for_better_sqlite3";
